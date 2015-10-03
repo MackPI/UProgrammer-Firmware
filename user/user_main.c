f15e65c83e2d8e2c337d141b798d3ebeb5bb9c8b
Steve McNeil <mack@prototypeiteration.com>
2015-10-03 00:35:22 -0700

    Hello World Firmware V0A

--- user/user_main.c
+++ user/user_main.c
@@ -42,17 +42,17 @@ void user_init(void)
 	struct station_config wifi_config;
     char ssid[32] = ""; // Access point name
 	char password[64] = ""; // Access point password
-//	os_install_putc1((void *)uart1_write_char);
-	os_memcpy(&wifi_config.ssid, ssid,32);
-	os_memcpy(&wifi_config.password, password,64);
-	wifi_config.bssid_set = 0;
 	uart_init(115200,115200);
-	system_set_os_print(1);
 	os_timer_disarm(&ptimer);
 	os_timer_setfn(&ptimer,user_state,NULL);
 	os_timer_arm(&ptimer,1000,1);
-	wifi_set_opmode(STATION_MODE);
-//	wifi_station_set_config(&wifi_config); // uncomment and add ssid/password to set up connection
+	if (ssid != ""){ // Only set up connection if firmware has connection Information
+		os_memcpy(&wifi_config.ssid, ssid,32);
+		os_memcpy(&wifi_config.password, password,64);
+		wifi_config.bssid_set = 0;
+		wifi_set_opmode(STATION_MODE);
+		wifi_station_set_config(&wifi_config);
+	}
 	wifi_set_event_handler_cb(wifi_handle_event_cb);
 //	at_register_uart_rx_intr();
 }
