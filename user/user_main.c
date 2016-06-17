dafe79a06bb0f5ae379f3b100cba9856c8569d93
Steve McNeil <mack@prototypeiteration.com>
2016-06-17 14:49:55 -0700

    Sigma Delta Testing/Cleanup V00E
    Made several changes to explicitly setup pins for sigma delta trying to
    improve operation.  Also removed some comments that are now irrelevant.
    Setting the sigma delta prescaler to a value larger than 99 causes a
    reboot. Still not fixed

--- user/user_main.c
+++ user/user_main.c
@@ -19,6 +19,8 @@
 #include "eagle_soc.h"
 #include "driver/sigma_delta.h"
 #include "driver/gpio16.h"
+#define FUNC_U0RXD                      0
+
 //#include "driver/spi_overlap.h"
 void user_rf_pre_init(void)
 {
@@ -27,30 +29,41 @@ void user_rf_pre_init(void)
 #define HSPI_PIN 2
 void user_init(void)
 {
-//	char byte_data[];
-//	hspi_overlap_init();
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO15); // GPIO15 = HSPICS
-//	GPIO_OUTPUT_SET(15, 1); // Disable SPI RAM
+	uint8 pin_index;
 	system_update_cpu_freq(80); //standard clock :)
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U,FUNC_GPIO13);
-//	GPIO_OUTPUT_SET(13, 1); // Disable Voltage Boost circuit
 
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U,FUNC_GPIO4);
-//    GPIO_OUTPUT_SET(4, 0);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0); // Analog Switch
+	GPIO_OUTPUT_SET(0, 0); //High= Vt Low = Vpp
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD); // Uart0 Controlled by API
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO2); //Vpp High Drive
+	GPIO_OUTPUT_SET(2, 1); //Set High to output VPP
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0RXD_U, FUNC_U0RXD); // Uart0 Controlled by API
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U,FUNC_GPIO4); // ΣΔ pulse stream for Voltage pump
+    GPIO_OUTPUT_SET(4, 1);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U,FUNC_GPIO5); //Vpp Low Drive
+    GPIO_OUTPUT_SET(5, 1); //Set High to GND VPP
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CLK_U,FUNC_SPICLK); //GPIO6 SPI SCLK
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA0_U,FUNC_SPIQ); //GPIO7 SPI MISO
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA1_U,FUNC_SPID); //GPIO8 SPI MOSI
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA2_U,FUNC_SPIHD); //GPIO9 SPI QIO2
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA3_U,FUNC_SPIWP); //GPIO10 SPI QIO3
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CMD_U,FUNC_SPICS0); //GPIO11 SPI CS0
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U,FUNC_GPIO12); //Target data pin
+    GPIO_OUTPUT_SET(12, 0);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U,FUNC_GPIO13); //Target data pin
+	GPIO_OUTPUT_SET(13, 0); // Set to known state
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U,FUNC_GPIO14); //Target data pin
+	for (pin_index = 0;pin_index <15;pin_index++){
+		GPIO_REG_WRITE((pin_index*4)+0x60000328,0); // disconnect all pins from ΣΔ
+	}
+
 	hspi_overlap_init();
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U,HSPI_PIN); // GPIO14
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U,HSPI_PIN); // GPIO12
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U,HSPI_PIN);// GPIO13
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U,HSPI_PIN);// GPIO15
 //	CLEAR_PERI_REG_MASK(PERIPHS_IO_MUX, BIT9);
 	writeRam();
 	serial_init();
-//	while(true); // infinite loop
 	wifi_set_event_handler_cb(wifi_handle_event_cb);
 
 
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U,FUNC_GPIO2);
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U,FUNC_GPIO5);
 
 /* having problems every time I try to add GPIO16
  * Moved voltage boost enable to GPIO13 for further testing
@@ -58,13 +71,6 @@ void user_init(void)
 //	gpio16_output_set(1); // Disable voltage boost circuit
 //	gpio16_output_conf();
 
-//	GPIO_OUTPUT_SET(5, 0); //Set High to GND VPP
-//	GPIO_OUTPUT_SET(2, 1); //Set High to output VPP
 	config_sigma_delta();
 
-//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, 2); // GPIO15 = HSPICS
-//	GPIO_OUTPUT_SET(15, 1); // Disable SPI RAM
-//	hspi_overlap_init();
-//	initSpiRam();
-//	readRam();
 }
